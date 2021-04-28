63ee3ba00a22c763d35a02fec6060c7edd0bed6c
Kevin Lai <knkevinlai@gmail.com>
2021-04-28 22:29:23 +0800

    feat: setup lint-staged eslint stylelint prettier import-sort yarn-deduplicate optipng

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,55 @@
-// TODO prettier
+/* eslint-disable */
 module.exports = {
-  "env": {
+  env: {
     node: true,
-    "browser": true,
-    "es2021": true
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:unicorn/recommended",
-    "plugin:prettier/recommended",
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint"
-  ],
-  "rules": {
-    "react/prop-types": ["error", { skipUndeclared: true }],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/prop-types': ['error', { skipUndeclared: true }],
     // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     // unicorn
-    "unicorn/prevent-abbreviations": "off",
+    'unicorn/prevent-abbreviations': 'off',
   },
   overrides: [
     {
-      files: ['**/commitlint.config.js'],
+      files: [
+        'commitlint.config.js',
+        'stylelint.config.js',
+        'lint-staged.config.js',
+        'importsort.config.js',
+        'importSortPrefix.config.js',
+      ],
       rules: {
         'unicorn/prefer-module': 'off',
+        'unicorn/filename-case': 'off',
       },
     },
   ],
---
+++ importsort.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '.js': {
+    style: 'module-and-prefix',
+  },
+  '.ts, .tsx': {
+    parser: 'typescript',
+    style: 'module-and-prefix',
+  },
+};
